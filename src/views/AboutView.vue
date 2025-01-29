d49516af6ff323f8606142adc49a4831023b0634
Olarn Rojanapornpun <olarnr@users.noreply.github.com>
2025-01-29 22:26:36 +0700

    initial release

---
+++ src/views/AboutView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>66130500210 Olarn Rojanapornpun</h1>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>INT210 Architecture, Integration, Deployment 2024</h1>
+  </div>
+</template>
